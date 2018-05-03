bac5534d2c34c5d08fa69f21592e80da68c09360
CraftedStone16 <stonedcraft16@gmail.com>
2018-05-03 16:00:05 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -53,7 +53,6 @@ client.on('message', async message => {
       message.delete(5000); 
       // message.channel.send('it hpned');
       return message.member.addRole('437738324183089154');
-      return;
     }
 });
 client.on('message', async message => {
