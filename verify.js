a1056977702e6ac9f4aa5325426af0aa100d144b
CraftedStone16 <stonedcraft16@gmail.com>
2018-05-23 12:32:53 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -667,8 +667,8 @@ client.on('message', async message => {
       .setColor('RANDOM')
       .addField('New Poll!', `${pollname}\n\nTo vote just simply react with 'Y'for Yes or 'N' for No!`)
       
-      client.channels.get(`${pollchannel}`).send('@everyone')
-      client.channels.get(`${pollchannel}`).send(poll).then(function (message) {
+      // client.channels.get(`${pollchannel}`).send('@everyone')
+      client.channels.get(`${pollchannel}`).send('@everyone'{poll}).then(function (message) {
                     message.react("🇾")
                     message.react("🇳")
                   })
