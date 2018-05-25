4e138f505b5d77c575cbacec30f278f1ded058c4
CraftedStone16 <stonedcraft16@gmail.com>
2018-05-25 12:57:38 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -25,13 +25,13 @@ var logs = '437757021953982485' // logging channel
 var configc = '437756098573893642' // Config Channel
 
 // <-=-> Role Variables
-var botowner = guild.roles.find('name', 'Bot Owner');
-var modRole = guild.roles.find('name', 'Moderator');
-var supportRole = guild.roles.find('name', 'Support Team');
-var premiumRole = guild.roles.find('name', 'Premium');
-var memberRole = guild.roles.find('name', 'Member');
-var verified = guild.roles.find('name', 'Verified');
-var muteRole = guild.roles.find('name', 'Muted');
+var botowner = client.roles.find('name', 'Bot Owner');
+var modRole = client.roles.find('name', 'Moderator');
+var supportRole = client.roles.find('name', 'Support Team');
+var premiumRole = client.roles.find('name', 'Premium');
+var memberRole = client.guild.roles.find('name', 'Member');
+var verified = client.guild.roles.find('name', 'Verified');
+var muteRole = client.guild.roles.find('name', 'Muted');
 
 var prefix = "--"
 var p = "--"
