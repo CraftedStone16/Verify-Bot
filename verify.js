38488b05f6421fd91dacd0b83fa1a82ce9a834ce
CraftedStone16 <stonedcraft16@gmail.com>
2018-05-25 19:57:57 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -488,7 +488,7 @@ client.on('message', async message => {
       if(!args) {
         return reply("Give me something to search idiot!");
       }
-      fecth.get("http://api.urbandictionary.com/v0/define?term" + args).then(res => {
+      fetch.get("http://api.urbandictionary.com/v0/define?term" + args).then(res => {
         if(res.body.list[0] === undefined) {
          return msg.channel.send("Could not find that term");
         }
@@ -496,17 +496,17 @@ client.on('message', async message => {
         const word = res.body.list[0].word;
         const Author = res.body.list[0].author;
         const exam = res.body.list[0].example;
-        const thump = res.body.list[0].thumbs_up;
+        const thumpup = res.body.list[0].thumbs_up;
         const thumbdown = res.body.list[0].thumbs_down;
         const embed = new Discord.RichEmbed()
         .setColor(hexcols[~~(Math.random() * hexcols.length)])
         .setTitle(`Info on the word: **${word}**`)
-        .addField("Definition:", `${defenition}`)
-        .addField("Author:", `${author}`)
+        .addField("Definition:", `${definition}`)
+        .addField("Author:", `${Author}`)
         .addField("Example:", ~`${exam}`)
         .addField("Ratings", `:thumbsup: ${thumbup} :thumbsdown: ${thumbdown}`, true)
-        .setThumbnail("Here is your search results", msg.author.displayAvatarURL);
-        msg.channel.send({embed}).catch(e => logger.error(e));
+        .setThumbnail("Here is your search results", message.author.displayAvatarURL);
+        message.channel.send({embed}).catch(e => logger.error(e));
       }).catch(err => {
         if(err) {}
       });
