c3acadff130ea128a82d890f216ef9451348990f
CraftedStone16 <stonedcraft16@gmail.com>
2018-06-12 02:37:42 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -63,9 +63,10 @@ client.on('message', async message => {
   
     if (message.content.startsWith(modprefix + 'quit')) {
       if(message.member.roles.has(botowner.id)) {
-         message.channel.send('Shutting Down now...').then(process > process.exit())
+         message.channel.send('Shutting Down now...')
+         process.exit();
       } else {
-        message.channel.send('You do not have the permission to use that command!')
+         message.channel.send('You do not have the permission to use that command!')
       }
     } else
 });
