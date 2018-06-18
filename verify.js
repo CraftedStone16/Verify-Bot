6c6709671a86ebd814b6ee542bba11f8edb985d9
CraftedStone16 <stonedcraft16@gmail.com>
2018-06-18 14:56:31 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -475,10 +475,9 @@ client.on('message', async message => {
       let args = message.content.split(' ').slice(1);
       let bug = args.join(' ')
       if (bug.length < 1) return message.channel.send('You must provide a message');
-      client.channels.get('415280541760356369').send(`<@&415260857786695711>, **New Bug:**\n\n${bug}`)
+      client.channels.get('458359176431796244').send(`<@&441648649395765259>, **New Bug:**\n\n${bug}`)
       message.author.send(`You just submitted a bug!\n\n\`${bug}\`\nWe appreciate all bugs that we get!`)
       message.channel.send(':ok_hand: I have reported your bug.')
-      client.channels.get(`${logs}`).send(`**${message.author.username}** just used the \`bug\` command in <#${message.channel.id}>!`)
     } else
 
     // Fun Commands
