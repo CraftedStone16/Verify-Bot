1cfe579fbd401699d443097ebbfa9780fc3d7b65
CraftedStone16 <stonedcraft16@gmail.com>
2018-08-06 13:09:48 -0500

    Update verify.js

--- verify.js
+++ verify.js
@@ -10,6 +10,7 @@ const cooldown = new Set();
 const fetch = require("snekfetch");
 const hexcols = [`#4285F4, #2D313C, #ffd700`];
 require('./util/eventLoader')(client);
+require('./events');
 client.login(process.env.BOT_TOKEN);
 
 
