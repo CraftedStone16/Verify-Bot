6c2112dbfadc1e3af292ff5362ef60ad693b1bf3
CraftedStone16 <stonedcraft16@gmail.com>
2018-08-07 00:08:27 -0500

    Update fuckyou.js

--- events/fuckyou.js
+++ events/fuckyou.js
@@ -1,12 +1,14 @@
 var prefix = "="
 module.exports = message => {
     if (message.content.startsWith(prefix + 'fy')) {
+      let args = message.content.split(' ').slice(1);
       let fymention = args.join(' ')
       if (fymention.length < 1) return message.reply('You must mention someone first.');
       message.channel.send(`${fymention}, Fuck You!`)
     } else
 
     if (message.content.startsWith(prefix + 'fuckyou')) {
+      let args = message.content.split(' ').slice(1);
       let fymention = args.join(' ')
       if (fymention.length < 1) return message.reply('You must mention someone first.');
       message.channel.send(`${fymention}, Fuck You!`)
