ac037916ed7a798aeb7bf829f063c2d4228cdfa5
CraftedStone16 <stonedcraft16@gmail.com>
2018-08-08 23:21:35 -0500

    Create setgame.js

---
+++ events/setgame.js
@@ -0,0 +1,28 @@
+var prefix = "="
+module.exports = message => {
+    if (message.content.startsWith(modprefix + 'sg')) {
+      if(message.member.roles.has(botowner.id)) {
+        client.user.setActivity(result);
+        let embed2 = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setTitle(`**New Game:** "${result}"`)
+        message.channel.send(embed2)
+      } else {
+        message.channel.send('You do not have the permission to use that command!')
+        client.channels.get(`${logs}`).send(`**${message.author.username}** just tried using the \`sg\` command in <#${message.channel.id}>!`)
+      }
+    } else
+
+    if (message.content.startsWith(modprefix + 'setgame')) {
+      if(message.member.roles.has(botowner.id)) {
+        client.user.setActivity(result);
+        let embed2 = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setTitle(`**New Game:** "${result}"`)
+        message.channel.send(embed2)
+      } else {
+        message.channel.send('You do not have the permission to use that command!')
+        client.channels.get(`${logs}`).send(`**${message.author.username}** just tried using the \`setgame\` command in <#${message.channel.id}>!`)
+      }
+    }
+};
