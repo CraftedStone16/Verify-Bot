e7fa6b82bf05a6a4a83c6bc0b26480c7070d3dd9
CraftedStone16 <stonedcraft16@gmail.com>
2018-08-08 23:23:26 -0500

    Create clean.js

---
+++ events/clean.js
@@ -0,0 +1,24 @@
+var prefix = "="
+module.exports = message => {
+    if (message.content.startsWith(modprefix + 'purge')) {
+      if(message.member.roles.has(modRole.id)) {
+        let messagecount = parseInt(result);
+        message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+        message.channel.send(`Deleted **${messagecount}** messages.`).then(message => message.delete(2500));
+      } else {
+        message.channel.send('You do not have the permission to use that command!')
+        client.channels.get(`${logs}`).send(`**${message.author.username}** just tried using the \`purge\` command in <#${message.channel.id}>!`)
+      }
+    } else
+
+    if (message.content.startsWith(modprefix + 'prune')) {
+      if(message.member.roles.has(modRole.id)) {
+        let messagecount = parseInt(result);
+        message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+        message.channel.send(`Deleted **${messagecount}** messages.`).then(message => message.delete(2500));
+      } else {
+        message.channel.send('You do not have the permission to use that command!')
+        client.channels.get(`${logs}`).send(`**${message.author.username}** just tried using the \`prune\` command in <#${message.channel.id}>!`)
+      }
+    }
+};
