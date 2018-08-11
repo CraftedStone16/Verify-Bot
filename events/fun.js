d665124795708f4ada454653da5f30984ffd0aa7
CraftedStone16 <stonedcraft16@gmail.com>
2018-08-10 22:28:03 -0500

    Update fun.js

--- events/fun.js
+++ events/fun.js
@@ -20,12 +20,52 @@ module.exports = message => {
     } else
 
     if (message.content.startsWith(prefix + '8ball')) {
-      let args = message.content.split(' ').slice(1);1
+      let args = message.content.split(' ').slice(1);
       if (!args[2]) return message.channel.send('Please ask a full question!');
       let replies = ["Yes.", "No.", "I don't know", "Ask Again Later"];
 
       let result = Math.floor((Math.random() * replies.length));
       let question = args.slice(0).join(" ");
+      message.channel.send(`${replies[result]}`)
+    } else
+    
+    if (message.content === `${prefix}gamble`) {
+      let args = message.content.split(' ').slice(1);
+      let replies = [
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "You Lose!",
+      "**YOU WIN!!**\n Please Ask Opensource what your prize is!"];
+
+      let result = Math.floor((Math.random() * replies.length));
       message.channel.send(`${replies[result]}`)
     }
 };
