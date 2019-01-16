c7f8e5c26766a345af93c5c4343f8723f6d5b1fa
CraftedStone16 <stonedcraft16@gmail.com>
2019-01-15 22:25:19 -0600

    Update verify.js

--- verify.js
+++ verify.js
@@ -304,6 +304,15 @@ client.on('message', async message => {
       message.channel.send('NOTHING!')
       message.channel.send('NOTHING!')
       message.channel.send('NOTHING!')
+    } else
+    
+    if (message.content.startswith(prefix + 'bug')) {
+      let args = message.content.split(' ').slice(1);
+      let bug = args.join(' ')
+      if (bug.length < 1) return message.channel.send('You must provide a message');
+      client.channels.get('458359176431796244').send(`<@&441648649395765259>, **New Bug:**\n\n${bug}`)
+      message.author.send(`You just submitted a bug!\n\n\`${bug}\`\nWe appreciate all bugs that we get!`)
+      message.channel.send(':ok_hand: I have reported your bug.')
     }
 });
 
