a2f88cb30054dbc1109901ec030556c43bbd4333
CraftedStone16 <stonedcraft16@gmail.com>
2019-01-15 22:10:59 -0600

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -7,6 +7,7 @@ module.exports = client => {
   console.log(`Bot Prefix: "${prefix}"`);
   console.log('All commands Loaded!');
   console.log(chalk.bgWhite.black('Your bot is now online (Verify Bot)'));
-  client.user.setActivity('Papa is gay and affraid to show it!', { type: 'STREAMING', url: 'https://www.opensource.black' });
+  //client.user.setActivity('Papa is gay and affraid to show it!', { type: 'STREAMING', url: 'https://www.opensource.black' });
+  client.user.setActivity('Version 4.0.0 is Live!');
   client.channels.get('448873575747944448').send('I have Restarted!');
 };
