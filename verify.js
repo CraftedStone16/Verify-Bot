3a4720dbe438712b01b74d65b61b49185b13ac85
CraftedStone16 <stonedcraft16@gmail.com>
2019-01-15 23:40:49 -0600

    Update verify.js

--- verify.js
+++ verify.js
@@ -345,6 +345,19 @@ client.on('message', async message => {
        .setFooter(`${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
        .setColor(16711728);
        messsage.client.channels.get(`${sticket}`).send(atembed)
+    } else
+    
+    if (message.content.startsWith(prefix + 'promote')) {
+       if(message.member.roles.has(memberRole.id)) {
+         let user = message.mentions.users.first();
+
+         if(!message.guild.member(client.user).hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) return message.reply('I do not have the correct permissions.').catch(console.error);
+
+         message.guild.member(user).addRole(memberRole.id)
+         client.channels.get(`${logs}`).send(`**${message.author.username}** just promoted **${user}**! [**Verified** to **Member**]`)
+       } else {
+         message.channel.send('You must have the Member role in order to promote that user to Member!')
+       }
     }
 });
 
