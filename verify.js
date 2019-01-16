f0752f6004fc50f9ba6dbde0197d731a1e199216
CraftedStone16 <stonedcraft16@gmail.com>
2019-01-15 23:39:52 -0600

    Update verify.js

--- verify.js
+++ verify.js
@@ -291,20 +291,6 @@ client.on('message', async message => {
         client.channels.get(`${logs}`).send(`**${message.author.username}** just tried using the \`unban\` command in <#${message.channel.id}>!`)
       }
     } else
-      
-    if (message.content.startsWith(prefix + 'remember')) {
-      message.channel.send('BITCH!! You know nothing!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-      message.channel.send('NOTHING!')
-    } else
     
     if (message.content.startsWith(prefix + 'bug')) {
       let args = message.content.split(' ').slice(1);
@@ -336,6 +322,29 @@ client.on('message', async message => {
       } else {
         message.channel.send('You do not have the permission to use that command!')
       }
+    } else
+    
+    if (message.content.startsWith(prefix + 'ticket')) {
+       if (!message.channel.id === `437739768223367168`) return;
+       if (!message.channel.id === `437739674577010688`) return;
+       if (message.channel.id === '437738261150957579') return;
+       message.delete();
+       let targs = message.content.split(' ').slice(1).join(' ');
+       if (targs.length < 1) return message.channel.send('You must provide a report for the ticket!');
+
+       let utembed = new Discord.RichEmbed()
+       .setAuthor(`${message.author.tag}`, message.author.displayAvatarURL)
+       .addField('Ticket Report:', `${targs}`)
+       .setColor("FFFFFF");
+       message.channel.send(utembed)
+       message.author.send(`Hey, ${message.author}, we got your report! We will reply soon as possible!`);
+
+       let atembed = new Discord.RichEmbed()
+       .setAuthor(`Ticket from ${message.author.tag}`, message.author.displayAvatarURL)
+       .addField('Full Report:', `${targs}`)
+       .setFooter(`${moment().format('MMMM Do YYYY, h:mm:ss a')}`)
+       .setColor(16711728);
+       messsage.client.channels.get(`${sticket}`).send(atembed)
     }
 });
 
